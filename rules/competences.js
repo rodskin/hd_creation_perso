765f6a004ba4aeeea383469206f6fe9615be15d8
fgilles <fgilles@corp.free.fr>
2021-03-15 23:31:27 +0100

    modification du calcul des caracteristiques avec variantes simples

---
+++ rules/competences.js
@@ -0,0 +1,92 @@
+const competences = {
+    'acrobatie': {
+        'key': 'acrobatie',
+        'label': 'Acrobatie',
+        'attribute': 'dex'
+    },
+    'arcanes': {
+        'key': 'arcanes',
+        'label': 'Arcanes',
+        'attribute': 'int'
+    },
+    'athletisme': {
+        'key': 'athletisme',
+        'label': 'Athlétisme',
+        'attribute': 'for'
+    },
+    'discretion': {
+        'key': 'discretion',
+        'label': 'Discretion',
+        'attribute': 'dex'
+    },
+    'dressage': {
+        'key': 'dressage',
+        'label': 'Dressage',
+        'attribute': 'sag'
+    },
+    'escamotage': {
+        'key': 'escamotage',
+        'label': 'Escamotage',
+        'attribute': 'dex'
+    },
+    'histoire': {
+        'key': 'histoire',
+        'label': 'Histoire',
+        'attribute': 'int'
+    },
+    'intimidation': {
+        'key': 'intimidation',
+        'label': 'Intimidation',
+        'attribute': 'cha'
+    },
+    'investigation': {
+        'key': 'investigation',
+        'label': 'Investigation',
+        'attribute': 'int'
+    },
+    'medecine': {
+        'key': 'medecine',
+        'label': 'Medecine',
+        'attribute': 'sag'
+    },
+    'nature': {
+        'key': 'nature',
+        'label': 'Nature',
+        'attribute': 'int'
+    },
+    'perception': {
+        'key': 'perception',
+        'label': 'Perception',
+        'attribute': 'sag'
+    },
+    'perspicacite': {
+        'key': 'perspicacité',
+        'label': 'Perspicacité',
+        'attribute': 'sag'
+    },
+    'persuasion': {
+        'key': 'persuation',
+        'label': 'Persuation',
+        'attribute': 'cha'
+    },
+    'religion': {
+        'key': 'religion',
+        'label': 'Religion',
+        'attribute': 'int'
+    },
+    'representation': {
+        'key': 'representation',
+        'label': 'Représentation',
+        'attribute': 'cha'
+    },
+    'supercherie': {
+        'key': 'supercherie',
+        'label': 'Supercherie',
+        'attribute': 'cha'
+    },
+    'survie': {
+        'key': 'survie',
+        'label': 'Survie',
+        'attribute': 'sag'
+    }
+}
---
+++ rules/historiques.js
@@ -0,0 +1,31 @@
+const historiques = {
+    'brigand': {
+        'key': 'brigand',
+        'label': 'Brigand',
+        'competences': [
+            'discretion',
+            'survie'
+        ],
+        'outils': [
+            'vehicules_terrestres',
+            'jeu'
+        ],
+        'equipement': [
+            'Couverture',
+            'Vêtements de voyageur',
+            'Piège à mâchoires',
+            'Boîte à amadou',
+            'Matériel de pêche',
+            'Outre d\'eau'
+        ],
+        'bourse': {
+            'pp': 0,
+            'po': 10,
+            'pa': 0,
+            'pc': 0
+        },
+        'aptitudes': {
+            0: "<strong>Signes de piste.</strong> Lorsque vous évoluez dans votre milieu naturel de prédilection, vous savez lire les multiples signes de la nature qui sont invisibles à un œil moins averti que le vôtre. De plus, vous pratiquez un langage codé qui permet de laisser des indices à ceux qui, comme vous, ont embrassé le brigandage. Cela vous permet d'éviter la plupart des dangers et des embûches qui guettent souvent le voyageur dans les régions les moins civilisées du monde, et de communiquer avec d'autres brigands sans attirer l'attention."
+        }
+    }
+}
