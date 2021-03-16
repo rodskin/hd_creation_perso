3ae7e805d410d7799ad6353e68c136ab6bd45b31
fgilles <fgilles@corp.free.fr>
2021-03-16 10:42:34 +0100

    ajout des langues

---
+++ rules/langues.js
@@ -0,0 +1,87 @@
+const langues = {
+    'commun' : {
+        'key': 'commun',
+        'label': 'Commun',
+        'alphabet': 'Commun'
+    },
+    'elfe': {
+        'key': 'elfe',
+        'label': 'Elfe',
+        'alphabet': 'Elfique'
+    },
+    'geant': {
+        'key': 'geant',
+        'label': 'Géant',
+        'alphabet': 'Runique'
+    },
+    'gnome': {
+        'key': 'gnome',
+        'label': 'Gnome',
+        'alphabet': 'Runique'
+    },
+    'gobelin': {
+        'key': 'gobelin',
+        'label': 'Gobelin',
+        'alphabet': 'Runique'
+    },
+    'halfelin': {
+        'key': 'halfelin',
+        'label': 'Halfelin',
+        'alphabet': 'Commun'
+    },
+    'nain': {
+        'key': 'nain',
+        'label': 'Nain',
+        'alphabet': 'Runique'
+    },
+    'orque': {
+        'key': 'orque',
+        'label': 'Orque',
+        'alphabet': 'Runique'
+    },
+    'abyssal': {
+        'key': 'abyssal',
+        'label': 'Abyssal',
+        'alphabet': 'Infernal'
+    },
+    'celeste': {
+        'key': 'celeste',
+        'label': 'Céleste',
+        'alphabet': 'Céleste'
+    },
+    'commun_profondeurs': {
+        'key': 'commun_profondeurs',
+        'label': 'Commun des profondeurs',
+        'alphabet': 'Elfique'
+    },
+    'draconique': {
+        'key': 'draconique',
+        'label': 'Draconique',
+        'alphabet': 'Draconique'
+    },
+    'infernal': {
+        'key': 'infernal',
+        'label': 'Infernal',
+        'alphabet': 'Infernal'
+    },
+    'primordial': {
+        'key': 'primordial',
+        'label': 'Primordial',
+        'alphabet': 'Runique'
+    },
+    'noir_parler': {
+        'key': 'noir_parler',
+        'label': 'Noir Parler',
+        'alphabet': '-'
+    },
+    'sselish': {
+        'key': 'sselish',
+        'label': 'Sselish',
+        'alphabet': '-'
+    },
+    'sylvestre': {
+        'key': 'sylvestre',
+        'label': 'Sylvestre',
+        'alphabet': 'Elfique'
+    }
+};
